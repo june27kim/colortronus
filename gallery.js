5d85b9d7e926edb1c466cfca357d797d95df2ea8
june27kim <kimberly27kim@gmail.com>
2016-11-16 06:25:56 +0300

    Fix gallery rows

--- gallery.js
+++ gallery.js
@@ -3,10 +3,10 @@ for (var i = 0; i < colors.length; i++) {
     var color = colors[i];
     var row = `
         <div class="row">
-            <div class='col-md-8' style="background-color: ${color.hex}; height:50px;">
+            <div class='col-md-6' style="background-color: ${color.hex}; height:50px;">
 
             </div>
-            <div class='col-md-4 color-name'>
+            <div class='col-md-6 color-name'>
                 ${color.ru_name}
             </div>
         </div>`;    
